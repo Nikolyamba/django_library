8f3f2935123ffc978ac1c907f679054a18bbfc1b
Nikolyamba <niko7tkc@yandex.ru>
2025-09-04 14:51:50 +0300

    little updates

--- library/urls.py
+++ library/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import users
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/api/register', users.views.register)
 ]
